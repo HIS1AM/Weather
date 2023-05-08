562e4ba3d82190b17511f3c67f20af7638c4e3f6
Mouhamad Hisham Bakri Radwan Mouhaffel <pshisham8@gmail.com>
2023-05-08 14:19:50 +0200

    Add project files.

---
+++ Weather/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Weather.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+
+<form runat="server">
+    <asp:TextBox ID="txtLocation1" runat="server"></asp:TextBox>
+    <br />
+   <asp:TextBox ID="txtLocation" runat="server"></asp:TextBox>
+    <br />
+   <asp:Label ID="lblTemperature" runat="server" Text="Label"></asp:Label>
+    <br />
+   <asp:Button ID="btnGetTemperature" runat="server" Text="Get Temperature" OnClick="btnGetTemperature_Click" />
+</form>
+
+</body>
+</html>
---
+++ Weather/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Weather
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        protected void btnGetTemperature_Click(object sender, EventArgs e)
+        {
+            // Get the location entered by the user
+            
+
+            // Call the getTemperature method to retrieve the temperature for the location
+            string temperature = getTemperature();
+
+            // Display the temperature on the web page
+            lblTemperature.Text = $"{temperature} &deg;C";
+        }
+
+        private string getTemperature()
+        {
+            string location0 = txtLocation.Text;
+            string location1 = txtLocation1.Text;
+            var http = new HttpClient();
+            http.DefaultRequestHeaders.Add("User-Agent", "Hisham");
+            var str = http.GetAsync($"https://api.met.no/weatherapi/nowcast/2.0/complete?lat={location0}&lon={location1}");
+            var result = str.Result.Content.ReadAsStringAsync().Result;
+            dynamic data = JObject.Parse(result.ToString());
+            string temperature = data.properties.timeseries[0].data.instant.details.air_temperature;
+            return temperature;
+        }
+
+    }
+}
+
+
